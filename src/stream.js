34047c5c33128043800c7e8b955f829ce1f3bff2
Vivek Kumar Singh <64981263+singhvivekkumar@users.noreply.github.com>
2023-07-29 21:13:40 +0530

    stream and transform

---
+++ src/stream.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const transformStream = require('stream');
+
+let fileStream = fs.createReadStream(__dirname+ '/input.txt')
+let outputStream = process.stdout;
+
+let middleStream = new transformStream.Transform({
+	transform(chunk, encoding, nextCallBack) {
+		let modifiedChunk = chunk.toString().toUpperCase();
+		this.push(modifiedChunk);
+		setTimeout(() => {
+			nextCallBack
+		}, 2000);
+	}
+})
+
+let newReadableStream = fileStream.pipe(middleStream);
+newReadableStream.pipe(outputStream);
